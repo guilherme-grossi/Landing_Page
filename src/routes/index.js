01843aee778a0546e13ded2ece551674c582753b
guilherme-grossi <guilhermegrossi1000@gmail.com>
2022-08-22 22:18:45 -0300

    erro not found 404

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/fazer-pedido', function(req, res, next) {
+  res.render('formulario', { title: 'Express' });
+});
+
 module.exports = router;
